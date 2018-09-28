99406486f4c4166bed208b1a796f43ce43f7aac6
Pal Dorogi <Pal.Dorogi@gmail.com>
2018-09-28 16:02:37 +1000

    Fixed includes files.

--- dafioc.pc.cmake
+++ dafioc.pc.cmake
@@ -1,4 +1,7 @@
+includedir=@DOLLAR@{prefix}/include
+
 Name: @PKGNAME@
 Description: DaF's IoC DI Module
 Version: @VERSION@
 Libs: -L@CMAKE_INSTALL_PREFIX@/lib -l@PKGNAME@
+Cflags: -I@CMAKE_INSTALL_PREFIX@/include/@PKGNAME@
