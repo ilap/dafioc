939e2c66a7eebe5aec208d15253062429b07a182
Pal Dorogi <pal.dorogi@gmail.com>
2018-10-09 17:14:37 +1000

    Refactored for using meson.

--- dafioc.pc.cmake
+++
@@ -1,7 +0,0 @@
-includedir=@DOLLAR@{prefix}/include
-
-Name: @PKGNAME@
-Description: DaF's IoC DI Module
-Version: @VERSION@
-Libs: -L@CMAKE_INSTALL_PREFIX@/lib -l@PKGNAME@
-Cflags: -I@CMAKE_INSTALL_PREFIX@/include/@PKGNAME@
